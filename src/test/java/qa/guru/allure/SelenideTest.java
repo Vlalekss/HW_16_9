9a32e3a41cddf9ea81fa147a7a7885e7a9035263
Vladislav Alekseev <vlad.alex14@gmail.com>
2023-01-23 19:15:49 +0500

    Initial commit

---
+++ src/test/java/qa/guru/allure/SelenideTest.java
@@ -0,0 +1,30 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testIssueSearch() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("Vlalekss/HW_16_8");
+        $(".header-search-input").submit();
+
+        $(linkText("Vlalekss/HW_16_8")).click();
+        $("#issues-tab").click();
+        $(withText("#1")).should(Condition.exist);
+    }
+
+}
